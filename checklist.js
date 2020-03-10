031e5475dc1326181c6ce69ac345c07052f253dd
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-03-09 19:24:43 -0700

    Fixed Word Wrap Bug

--- checklist.js
+++ checklist.js
@@ -7,44 +7,44 @@ let isEntryClicked = false;
 
 setup();
 
-function setup(){
+function setup() {
 	document.querySelector("#instructions").style.display = "none"
 }
 
-function minimizeInstructions(){
+function minimizeInstructions() {
 	document.querySelector("#instructions").classList.remove("zoomIn");
 	document.querySelector("#instructions").classList.add("zoomOut");
 	//gives the illusion that the instructions and to-do body moves swiftly together
 	slideUpToDoBody();
 }
 
-function slideUpToDoBody(){
+function slideUpToDoBody() {
 	const toDoBody = document.querySelector("#toDoBody");
 
 	toDoBody.classList.add("slideOutUpCustom");
-	toDoBody.addEventListener("animationend", function(){
+	toDoBody.addEventListener("animationend", function () {
 		toDoBody.classList.remove("slideOutUpCustom");
 	});
 }
 
-function zoomInAnimation(){
+function zoomInAnimation() {
 	//remove, in case zoomOut exists, due to previous clicks
 	document.querySelector("#instructions").classList.remove("zoomOut");
 	document.querySelector("#instructions").classList.add("zoomIn");
 }
 
-function hideInstructions(){
+function hideInstructions() {
 	minimizeInstructions();
 	document.querySelector("#instructions").addEventListener("animationend", onInstructionsZoomedOut);
 	areInstructionsShowing = false;
 }
 
-function onInstructionsZoomedOut(){
+function onInstructionsZoomedOut() {
 	document.querySelector("#instructions").style.display = "none";
 	document.querySelector("#instructions").removeEventListener("animationend", onInstructionsZoomedOut);
 }
 
-function showInstructions(){
+function showInstructions() {
 	//should I remove the event listener here?
 
 	zoomInAnimation();
@@ -54,24 +54,24 @@ function showInstructions(){
 }
 
 //if instructions title is click, it'll show instructions, vice versa
-document.querySelector("#instructionsTitle").onclick = function(){
-	if(areInstructionsShowing){
+document.querySelector("#instructionsTitle").onclick = function () {
+	if (areInstructionsShowing) {
 		hideInstructions();
 	} else {
 		showInstructions();
 	}
 };
 
-function grabInput(){
+function grabInput() {
 	task = document.querySelector("#inputField").value;
 }
 
-function clearInput(){
+function clearInput() {
 	document.querySelector("#inputField").value = "";
 }
 
-document.querySelector("#submitButton").onclick = function(){
-	if(document.querySelector("#inputField").value.length === 0){
+document.querySelector("#submitButton").onclick = function () {
+	if (document.querySelector("#inputField").value.length === 0) {
 		showAlert();
 		return;
 	}
@@ -80,39 +80,35 @@ document.querySelector("#submitButton").onclick = function(){
 	addEntry(task);
 };
 
-function addEntry(task){
+function addEntry(task) {
 	var newEntry = document.createElement("div");
-	var entryText = document.createTextNode(task);
-	
+	newEntry.classList.add("entryDesign", "animated", "zoomIn");
 	
 	//create button to delete entry
 	var button = document.createElement("button");
-	button.onclick = function(){
+	button.classList.add("fas", "fa-times", "float-right", "fixed-button", "buttonDesign");
+	button.onclick = function () {
 		newEntry.remove();
 	}
-	
+
 	//change color when user clicks entry
-	newEntry.onclick = function(){
-		if(isEntryClicked == false) {
+	newEntry.onclick = function () {
+		if (isEntryClicked == false) {
 			newEntry.style.backgroundColor = "#ffaf8b";
-			button.style.backgroundColor= "#ffaf8b";
+			button.style.backgroundColor = "#ffaf8b";
 			isEntryClicked = true;
-	} else {
+		} else {
 			newEntry.style.backgroundColor = "#ea7e5d";
-			button.style.backgroundColor= "#ea7e5d";
+			button.style.backgroundColor = "#ea7e5d";
 			isEntryClicked = false;
-	}
-};
-	
+		}
+	};
+
 	//icon for display inside button
 	var icon = document.createElement("i");
-	newEntry.classList.add("entryDesign", "animated", "zoomIn");
-	button.classList.add("fas", "fa-times", "float-right", "buttonDesign");
-	
 	button.appendChild(icon);
-	
-	
-	
+
+	var entryText = document.createTextNode(task);
 	newEntry.appendChild(entryText);
 	newEntry.appendChild(button);
 	document.querySelector("#entryContainer").appendChild(newEntry);
@@ -121,7 +117,7 @@ function addEntry(task){
 
 
 //show alert if no text in input field
-function showAlert(){
+function showAlert() {
 	bootbox.alert({
 		message: "Enter item to create a new task!",
 		size: 'small'
@@ -129,8 +125,8 @@ function showAlert(){
 }
 
 //press enter to trigger a submit button click
-document.querySelector("#inputField").addEventListener("keyup", function(event){
-	if(event.keyCode === 13){
+document.querySelector("#inputField").addEventListener("keyup", function (event) {
+	if (event.keyCode === 13) {
 		event.preventDefault;
 		document.querySelector("#submitButton").click();
 	}
